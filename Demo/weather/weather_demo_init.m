83ddc93da8bb0193ebbab7f8202092d0f81bcd55
Stephen Frank <Stephen.Frank@nrel.gov>
2013-06-26 19:39:23 +0000

    Added a weather demo.
    WARNING: This demo and it's accompanying block work in MATLAB R2013a only until some backwards compatibility issues with data import into Simulink can be addressed.

---
+++ Demo/weather/weather_demo_init.m
@@ -0,0 +1,22 @@
+%% Weather Demo Setup Script
+% This script sets up:
+%   1. Time series .MAT files of weather data from TMY3-formatted sources
+%   2. Simulink bus object definitions to allow proper I/O when using the
+%      'From File' block
+%
+% Run this script prior to running the Simulink simulation.
+
+%% Convert TMY3 Weather Files to MATLAB Time Series
+% Use convertTMY3() conversion utility
+tmy3 = convertTMY3('724666TY.csv', 'offset', 0);	% TMY3 data - full year
+month = convertTMY3('201206ty.csv', 'offset', 0);	% Actual data - 1 month
+
+% Save resulting structures of time series to file
+% Notes:
+%   1. The name 'ans' is required by Simulink to import data using a 'From
+%      File' block
+%   2. A version 7.3 .MAT file is required for Simulink to properly read
+%      the time series object. This is NOT the default version which MATLAB
+%      saves, so be careful.
+ans = tmy3;     save('TMY3.mat', 'ans', '-v7.3');
+ans = month;    save('Month.mat', 'ans', '-v7.3');

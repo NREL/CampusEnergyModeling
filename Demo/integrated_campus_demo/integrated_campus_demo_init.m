e50f5e4bd3928246d4e62a372c1dd6688d230cea
Stephen Frank <Stephen.Frank@nrel.gov>
2013-07-22 22:14:46 +0000

    Modified demo to have DataBus weather data as an input option.
    NOTE: Only partially tested; DataBus wasn't cooperating.

--- Demo/integrated_campus_demo/integrated_campus_demo_init.m
+++ Demo/integrated_campus_demo/integrated_campus_demo_init.m
@@ -1,7 +1,16 @@
 %% Integrated Campus Demo Initialization Script
-% This script initializes the demo by creating some required input files.
+% This script initializes the demo by creating required input files for the
+% weather data. Note that downloading DataBus data is an interactive
+% process requiring the user to save files from a web browser to .CSV and
+% pass the resulting file name back to MATLAB.
 %
-% Run this script prior to running the Simulink simulation.
+% Run this script prior to running the Simulink simulation. (You only need
+% to run it once; the necessary files will persist afterwards.)
+%
+% To switch to DataBus data instead of the included TMY3 data, modify the
+% variables as needed under the 'Retrieve DataBus Weather Data' section and
+% re-run the script. Also, remember to change the weather block in Simulink
+% to point to 'Weather2.mat'.
 
 %% Initialize Weather Data
 % Pulls in TMY3-formatted weather file for June 2012 in Golden, CO
@@ -17,3 +26,47 @@ convertTMY3('201206ty.csv', 'offset', 0);
 %      the time series object. This is NOT the default version which MATLAB
 %      saves, so be careful.
 save('Weather.mat', 'ans', '-v7.3');
+
+%% Retrieve DataBus Weather Data
+% Pulling data from DataBus into MATLAB is tricky; direct methods fail for
+% cryptic and unsolveable reasons:
+%   urlread()       SSL errors and authentication errors (even w/ correct
+%                   credentials)
+%   web()           Locks up MATLAB when using MATLAB's internal browser
+%   
+% Instead, this script uses an interactive approach which prompts the user
+% to download each data stream to .CSV, then parses the result.
+
+% Change to 'true' to download and use DataBus data
+useDataBus = false;
+
+% Time stamps for data to retrieve (dd-mmm-yyyy HH:MM:SS)
+start = '01-Jun-2012 00:00:00';
+stop  = '02-Jun-2012 00:00:00';
+
+% Run the interactive function to retrieve and use DataBus data
+if useDataBus
+    % Interactive import from databus
+    x = importDataBus('DataBus_sensors.csv', start, stop, 'timezone', -7);
+    
+    % This puts the result into 'ans'
+    x;
+
+    % Save resulting structures of time series to file
+    % Notes:
+    %   1. The name 'ans' is required by Simulink to import data using a 'From
+    %      File' block
+    %   2. A version 7.3 .MAT file is required for Simulink to properly read
+    %      the time series object. This is NOT the default version which MATLAB
+    %      saves, so be careful.
+    save('Weather2.mat', 'ans', '-v7.3');
+    
+    % NOTE: Remember to switch to 'Weather2.mat' in the Simulink model!
+end
+
+% TEST: Plot the resulting time series
+% hold on
+% plot(x.GHI,'-b')
+% plot(x.DNI,'-r')
+% plot(x.DHI,'-g')
+% hold off

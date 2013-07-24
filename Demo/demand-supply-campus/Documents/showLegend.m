a384d0719eea4ce4b1f2db7606ef6be0598e76a0
Willy Bernal <Willy.Bernal@nrel.gov>
2013-07-24 23:20:16 +0000

    deleted demand-supply-campus demo.

--- Demo/demand-supply-campus/Documents/showLegend.m
+++
@@ -1,3 +0,0 @@
-hScopes = findall(0,'Tag','SIMULINK_SIMSCOPE_FIGURE','Name', 'Mass & Flow Partition')
-hAxes = findall(hScopes(1),'Type','axes');
-legend(hAxes(1),{'Building 2','Building 1','Total'},'Color',[1 1 1]);

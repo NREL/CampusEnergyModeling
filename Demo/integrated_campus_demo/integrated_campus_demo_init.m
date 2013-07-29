ef1a426a0387cb9507161a29b531f7503a69cf0e
unknown <wbernal@bball-129913.nrel.gov>
2013-07-29 09:29:58 -0600

    Added DR controls subject to Price signal.

--- Demo/integrated_campus_demo/integrated_campus_demo_init.m
+++ Demo/integrated_campus_demo/integrated_campus_demo_init.m
@@ -70,6 +70,51 @@ if useDataBus
     
 end
 
+%% Initialize Pricing Data
+% Loads Day-Ahead Pricing from June, 2013 from PJM.
+
+% Load Array from CSV File
+day = 1;
+M = csvread('201306-da.csv',5+day,1,[5+day 1 5+day 24])/1000';
+% Price is for $/KWh
+x = M;
+t = [0:23]*60*60;
+priceDA = timeseries(x, t);
+
+% Assign time series time properties
+priceDA.TimeInfo.Units = 'seconds';
+% priceDA.TimeInfo.StartDate = start;
+% priceDA.TimeInfo.Format = tFormat;
+
+% Assign time series data properties
+priceDA.Name = 'priceDA';
+priceDA.DataInfo.Units = '$/KWh';
+priceDA.DataInfo.UserData = 'Total LMP ($/KWh)';
+
+% % Create Bus
+% 
+% % Create the bus
+% BusDef_Price = Simulink.Bus;
+% BusDef_Price.Description = 'LMP DAY-AHEAD';
+% 
+% % Populate the bus
+% % Create bus element w/ proper names, etc
+% x = Simulink.BusElement;
+% x.Name = 'priceDA';
+% x.DocUnits = out.priceDA.DataInfo.Units;
+% x.Description = out.priceDA.DataInfo.UserData;
+% 
+% % Store in bus
+% BusDef_Price.Elements = x;
+% 
+% % Place in base workspace
+% assignin('base', 'BusDef_Price', BusDef_Price);
+
+% % Clear local copy
+% clear('BusDef_Price');
+
+save('PriceDA.mat', 'priceDA', '-v7.3');
+
 % TEST: Plot the resulting time series
 % hold on
 % plot(ans.GHI,'-b')

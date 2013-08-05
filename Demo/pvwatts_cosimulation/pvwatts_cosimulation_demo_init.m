3ffd0c1a4ffd5b3206b565ddd773097aee98ff15
Stephen Frank <Stephen.Frank@nrel.gov>
2013-08-05 17:34:36 -0600

    1. Added a PV smoothing model (low pass filter) 2. Updated PVWatts demo to demonstrate this smoothing model. (README changes to follow.)

--- Demo/pvwatts_cosimulation/pvwatts_cosimulation_demo_init.m
+++ Demo/pvwatts_cosimulation/pvwatts_cosimulation_demo_init.m
@@ -1,10 +1,87 @@
 %% PVWatts Cosimulation Demo Initialization Script
-% This script initializes the demo by creating a time series .MAT files of
-% weather data from a TMY3-formatted source.
+% This script initializes the PVWatts cosimulation demo. Both short-term
+% and long-term scenarios are available. The scenarios differ by weather
+% data and simulation settings:
 %
-% Run this script prior to running the Simulink simulation.
+% 1. Short-term: This scenario models 24 hours of PV operation at a 10
+%    second time step. The weather data for this scenario will be
+%    downloaded from DataBus using the interactive utility function
+%    importDataBus() when this script executes; please read and follow the
+%    prompts in the MATLAB command window.
+%
+% 2. Long-term: This scenario models 1 month of PV operation at a 10 minute
+%    time step. The weather data is drawn from a TMY3-formatted weather
+%    file (included with this demo).
+%
+% Modify the user settings below to select a scenario, then run this script
+% prior to running the Simulink simulation. (You only need to run it once; 
+% the necessary files and settings will persist afterwards.)
+%
+% NOTE: The DC and AC powers are output as vectors to the MATLAB workspace.
+% You can plot the inverter efficiency characteristic using:
+%    eff = ac_power ./ dc_power;
+%    eff( ac_power <= 0) = 0;
+%    plot(dc_power, eff);
+% This plot can also be created directly in Simulink, but it severly slows
+% down the simulation time.
 
-%% Convert TMY3 Weather Files to MATLAB Time Series
-% Use convertTMY3() conversion utility
-convertTMY3('201206ty.csv', 'offset', 0);
-save('Weather.mat', 'ans', '-v7.3'); 
+%% User Settings
+% Scenario selection: please specify either 'short' or 'long'
+scen = 'long';
+
+%% Check Inputs
+% Check for proper scenario
+assert( any( strcmpi(scen, {'short','long'}) ), ...
+    'CampusModeling:demo:invalidScenario', ...
+    ['No scenario named ''' scen ''' exists; ' ...
+    'cannot initialize the demo.']);
+
+%% Initialize Weather Data
+% Data source depends on scenario
+switch scen
+    % Short-term
+    case 'short'
+        % Time stamps for data to retrieve (dd-mmm-yyyy HH:MM:SS)
+        start = '01-Jun-2013 00:00:00';
+        stop  = '02-Jun-2013 00:00:00';
+        
+        % Interactive import from databus -> result in 'ans' variable
+        importDataBus('DataBus_sensors.csv', start, stop, 'timezone', -7);
+        save('Weather.mat', 'ans', '-v7.3');
+    
+    % Long-term
+    case 'long'
+        % Use convertTMY3() conversion utility
+        convertTMY3('201206ty.csv', 'offset', 0);
+        save('Weather.mat', 'ans', '-v7.3'); 
+        
+end
+
+%% Initialize Simulink Model
+% Open it if not open
+pvwatts_cosimulation;
+
+% Set timestep and simulation duration
+switch scen
+    % Short-term
+    case 'short'
+        timestep = 1;
+        PVtimestep = 10;
+        dur = 3600*24;
+    
+    % Long-term
+    case 'long'
+        timestep = 15;
+        PVtimestep = 600;
+        dur = 30 * (3600*24);
+        
+end
+
+% Set settings and block parameters
+set_param('pvwatts_cosimulation', ...
+    'StartTime', num2str(0), ...
+    'StopTime', num2str(dur), ...
+    'FixedStep', num2str(timestep) );
+set_param('pvwatts_cosimulation/PVWatts Cosimulation', ...
+    'time_step', num2str(PVtimestep) );
+save_system;
---
+++ Library/R2013a/PVSmoothing_cb.m
@@ -0,0 +1,122 @@
+%% Function: Implements callbacks for 'PV Smoothing' block
+% This function implements the mask callbacks for the 'PV Smoothing' block
+% in the NREL Campus Modeling Simulink block library. It is designed to be
+% called from the block mask.
+%
+% SYNTAX:
+%   varargout = ConstantPowerSourceLoad_cb(block, callback, varargin)
+%
+% INPUTS:
+%   block =     Simulink block path
+%   callback =  String specifying the callback to perform; see code
+%   varargin =  Inputs which vary depending on the callback; see code
+%
+% OUTPUTS:
+%   varargout = Outputs which vary depending on the callback
+%
+% COMMENTS:
+% 1. This function is not intended for use outside of the NREL Campus
+%    Modeling Simulink library; therefore the error checking and
+%    documentation are minimal. View the code to see what is going on.
+
+function varargout = PVSmoothing_cb(block, callback, varargin)
+    %% Setup
+    % Default output = none
+    varargout = {};
+
+    %% Callbacks
+    % Select and execute desired callback
+    switch callback
+        % Initialization
+        case 'init'
+            varargout = PVSmoothing_cb_init(block, varargin{:});
+        
+        % Dynamic mask dialog
+        case 'dynamic_mask'
+            PVSmoothing_cb_dynamicmask(block);
+            
+        otherwise
+            warning([block ':unimplementedCallback'], ...
+                ['Callback ''' callback ''' not implemented.']);
+        
+    end
+end
+
+%% Subfunctions
+% Initialization
+function out = PVSmoothing_cb_init(block, sel, ...
+    tc, area, size, density, coverage)
+    % Switch based on what is specified
+    switch sel
+        % Specify time constant
+        case 1
+            % Check and copy to output
+            %assert(tc > 0, [block ':incorrectParameter'], ...
+            %    'Specified time constant must be strictly positive.');
+            tau = tc;
+        
+        % Specify array size
+        case 2
+            % Check input
+            assert(area > 0, [block ':incorrectParameter'], ...
+                'Array area must be strictly positive.');
+            
+            % Convert to hectares (Ha)
+            area = area / 10000;
+            
+            % Calculate time constant based on empirical formulate from
+            % Marcos et al. (2011)
+            tau = sqrt(area) / (2*pi*0.020);
+            
+        % Specify array area and power density
+        case 3
+            % Check inputs
+            assert(size > 0, [block ':incorrectParameter'], ...
+                'Array size must be strictly positive.');
+            assert(density > 0, [block ':incorrectParameter'], ...
+                'Panel power density must be strictly positive.');
+            assert(coverage > 0, [block ':incorrectParameter'], ...
+                ['Panel coverage ratio must be greater than zero and ' ...
+                'less than or equal to one.']);
+            
+            % Estimate array area (Ha)
+            area = size / density / coverage / 10000;
+            
+            % Calculate time constant based on empirical formulate from
+            % Marcos et al. (2011)
+            tau = sqrt(area) / (2*pi*0.020);
+    end
+    
+    % Return computed variables in cell array (compatible w/ varargout)
+    out = {tau};
+end
+
+% Dynamic mask dialog
+function PVSmoothing_cb_dynamicmask(block)
+    % Get block info
+    vis = get_param(block, 'MaskVisibilities');
+    sel = get_param(block, 'sel');
+    
+    % Set visibilities
+    % See mask parameter dialog for number-to-name matching
+    switch sel
+        case 'Time Constant'
+            vis(2) = {'on'};
+            vis(3) = {'off'};
+            vis(4:6) = {'off'};
+        
+        case 'Array Area'
+            vis(2) = {'off'};
+            vis(3) = {'on'};
+            vis(4:6) = {'off'};
+        
+        case 'Array Size and Power Density'
+            vis(2) = {'off'};
+            vis(3) = {'off'};
+            vis(4:6) = {'on'}; 
+    end
+
+    % Write visibilities to mask; match mask enables to visibilities
+    set_param(block, 'MaskVisibilities', vis);
+    set_param(block, 'MaskEnables', vis);
+end

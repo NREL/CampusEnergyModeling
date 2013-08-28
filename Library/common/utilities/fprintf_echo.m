990f9ee7c68bbc5e159e502d55df3f1483308584
Stephen Frank <Stephen.Frank@nrel.gov>
2013-08-28 16:45:49 -0600

    Added an automated test script that will pick up any tests in the directory structure underneath it and run them.

---
+++ Library/common/utilities/fprintf_echo.m
@@ -0,0 +1,27 @@
+%% FPRINTF_ECHO - Write to file via fprint() and echo to standard out
+%
+% Writes a message to one or more files using fprintf() and simultaneously
+% echos the message to the standard output.
+%
+% SYNTAX:
+%   fprintf_echo(fid, varargin)
+%
+% INPUTS:
+%   fid =           Vector of file identifiers for log output
+%   varargin =      All other arguments typically passed to fprintf
+%
+% EXAMPLES:
+%   % Writes 'test message 1' to files test1.log and test2.log
+%   f1 = fopen('test1.log','w');
+%   f2 = fopen('test2.log','w');
+%   fprintf_echo([f1 f2], '%s %d', 'test message', 1)
+%   fclose(f1);
+%   fclose(f2);
+
+function fprintf_echo(fid, varargin)
+    % Output to files
+    arrayfun(@(x) fprintf(x, varargin{:}), fid);
+    
+    % Output to standard out
+    fprintf( varargin{:} );
+end
---
+++ Test/run_tests.m
@@ -0,0 +1,307 @@
+%% run_tests.m - Run automated tests for Campus Energy Modeling library
+%
+% This script executes automated tests for the Campus Energy Modeling
+% library and logs the results.
+%
+% COMMENTS:
+% 1. To disable MATLAB test scripts, set
+%       runMatlab = false
+%    To disable Simulink test scripts, set
+%       runSimulink = false
+% 
+% 2. For all tests to run successfully, all the software dependencies for
+%    the Campus Energy Modeling library must be installed. Otherwise, some
+%    tests will fail when they attempt to call external commands that are
+%    not available. You can omit tests for specific dependencies in the
+%    configuration by specifying dependency names in the cell array
+%    'omitDeps'.
+%
+% 3. Set
+%       clearOldLogs = true
+%    to remove old test logs prior to executing the tests. Note that this
+%    does not clear the main log file but only the logs for each individual
+%    test.
+%
+% 4. Hardware-in-loop tests using RT-LAB cannot be run automatically and
+%    are not included in this script.
+
+%% Configuration
+% Run MATLAB tests?
+runMatlab = true;
+
+% Run Simulink tests?
+runSimulink = true;
+
+% Omit tests that require any of the following dependencies:
+omitDeps = {'EnergyPlus','MLEPlus'};
+
+% Valid dependency names are:
+%   energyplus
+%   mleplus
+%   simpowersystems
+%   ssc
+%
+% Names are not case sensitive; the script converts to lower case
+
+% Log file name
+logfile = 'run_tests.log';
+
+% Clear old logs?
+clearOldLogs = false;
+
+%% Setup
+% Open log file
+flog = fopen(logfile, 'a+t');
+
+% Log start of tests
+fprintf_echo(flog, '[%s] %s\n', ...
+    datestr(now, 'yyyy-mm-dd HH:MM:SS'), ...
+    'Starting automated tests for Campus Energy Modeling library' );
+
+% Report configuration
+fprintf_echo(flog, '[%s] %s\n', ...
+    datestr(now, 'yyyy-mm-dd HH:MM:SS'), ...
+    'Configuration:' );
+fprintf_echo(flog, '\tMATLAB tests: ');
+if runMatlab
+    fprintf_echo(flog, 'yes\n');
+else
+    fprintf_echo(flog, 'no\n');
+end
+fprintf_echo(flog, '\tSimulink tests: ');
+if runSimulink
+    fprintf_echo(flog, 'yes\n');
+else
+    fprintf_echo(flog, 'no\n');
+end
+fprintf_echo(flog, '\tSkipping tests with dependencies: %s\n', ...
+    strjoin(omitDeps,', ') );
+fprintf_echo(flog, '\tClearing old log files: ');
+if clearOldLogs
+    fprintf_echo(flog, 'yes\n');
+else
+    fprintf_echo(flog, 'no\n');
+end
+
+% Set up counters
+nPass = 0;
+nFail = 0;
+nSkip = 0;
+
+% Root directory for test script
+root = pwd;
+
+% Suppress unreachable code warnings:
+%#ok<*UNRCH>
+
+%% MATLAB Tests
+% Run MATLAB tests
+if runMatlab
+    % Log message
+    fprintf_echo(flog, '[%s] %s\n', ...
+        datestr(now, 'yyyy-mm-dd HH:MM:SS'), ...
+        'Running MATLAB tests:' );
+    
+    % Get contents of MATLAB test directory
+    % (Rows = name, date, bytes, isdir, datenum)
+    testSet = struct2cell( dir('./matlab') );
+    
+    % Filter to only diretories...
+    testSet = testSet(:, cell2mat(testSet(4,:)) );
+    
+    % ...with names that aren't . or ..
+    testSet = setdiff(testSet(1,:), {'.','..'});
+    
+    % Run each test
+	for i = 1:length(testSet)
+        % Change working directory
+        cd( [root filesep 'matlab' filesep testSet{i}] );
+        
+        % Test script name
+        n = testSet{i};
+        
+        % Clear old log file if requested
+        if clearOldLogs && exist([n '.log'], 'file')
+            delete( [n '.log'] );
+        end
+        
+        % Verify presence of test script; skip otherwise
+        if exist(n, 'file') ~= 2
+            fprintf_echo(flog, ...
+                '[%s] %s "%s" (%s)\n', ...
+                datestr(now, 'yyyy-mm-dd HH:MM:SS'), ...
+                'Warning: Skipping test directory', n, ...
+                'test script not found' );
+            continue    
+        end
+        
+        % Check for known dependencies
+        if exist('deps.cfg', 'file')
+            fid = fopen('deps.cfg','r');
+            deps = textscan(fid, '%s', 'Delimiter', ',');
+            fclose(fid);
+            deps = deps{:};
+        else
+            deps = {};
+        end
+        deps = intersect(lower(deps), lower(omitDeps));
+        
+        % Run...
+        if isempty(deps)
+            % Run test w/ log message
+            fprintf_echo(flog, '[%s] %s "%s.m"... ', ...
+                datestr(now, 'yyyy-mm-dd HH:MM:SS'), ...
+                'Running test', n);
+            ok = runTestScript(n);
+            if ok
+                status = 'success';
+            else
+                status = 'failure';
+            end
+            fprintf_echo(flog, '%s\n', status);
+                
+        % Skip...
+        else
+            fprintf_echo(flog, ...
+                '[%s] %s "%s.m" (%s %s)\n', ...
+                datestr(now, 'yyyy-mm-dd HH:MM:SS'), ...
+                'Skipping test', n, ...
+                'due to dependency', deps{1} );
+            ok = -1;
+        end
+        
+        % Log what happened
+        switch ok
+            case 1
+                nPass = nPass + 1;
+            case 0
+                nFail = nFail + 1;
+            case -1
+                nSkip = nSkip + 1;
+        end
+	end
+    
+    % Return to root folder
+    cd(root)
+    
+% Skip MATLAB tests
+else
+    % Log message
+    fprintf_echo(flog, '[%s] %s\n', ...
+        datestr(now, 'yyyy-mm-dd HH:MM:SS'), ...
+        'Skipping MATLAB tests' );
+end
+
+%% Simulink Tests
+% Run Simulink tests
+if runSimulink
+    % Log message
+    fprintf_echo(flog, '[%s] %s\n', ...
+        datestr(now, 'yyyy-mm-dd HH:MM:SS'), ...
+        'Running Simulink tests:' );
+    
+    % Get contents of MATLAB test directory
+    % (Rows = name, date, bytes, isdir, datenum)
+    testSet = struct2cell( dir('./simulink') );
+    
+    % Filter to only diretories...
+    testSet = testSet(:, cell2mat(testSet(4,:)) );
+    
+    % ...with names that aren't . or ..
+    testSet = setdiff(testSet(1,:), {'.','..'});
+    
+    % Run each test
+	for i = 1:length(testSet)
+        % Change working directory
+        cd( [root filesep 'matlab' filesep testSet{i}] );
+        
+        % Test script name
+        n = testSet{i};
+        
+        % Clear old log file if requested
+        if clearOldLogs && exist([n '.log'], 'file')
+            delete( [n '.log'] );
+        end
+        
+        % Verify presence of test script; skip otherwise
+        if exist(n, 'file') ~= 2
+            fprintf_echo(flog, ...
+                '[%s] %s "%s" (%s)\n', ...
+                datestr(now, 'yyyy-mm-dd HH:MM:SS'), ...
+                'Warning: Skipping test directory', n, ...
+                'test script not found' );
+            continue    
+        end
+        
+        % Check for known dependencies
+        if exist('deps.cfg', 'file')
+            fid = fopen('deps.cfg','r');
+            deps = textscan(fid, '%s', 'Delimiter', ',');
+            fclose(fid);
+            deps = deps{:};
+        else
+            deps = {};
+        end
+        deps = intersect(lower(deps), lower(omitDeps));
+        
+        % Run...
+        if isempty(deps)
+            % Run test w/ log message
+            fprintf_echo(flog, '[%s] %s "%s.m"... ', ...
+                datestr(now, 'yyyy-mm-dd HH:MM:SS'), ...
+                'Running test', n);
+            ok = runTestScript(n);
+            if ok
+                status = 'success';
+            else
+                status = 'failure';
+            end
+            fprintf_echo(flog, '%s\n', status);
+                
+        % Skip...
+        else
+            fprintf_echo(flog, ...
+                '[%s] %s "%s.m" (%s %s)\n', ...
+                datestr(now, 'yyyy-mm-dd HH:MM:SS'), ...
+                'Skipping test', n, ...
+                'due to dependency', deps{1} );
+            ok = -1;
+        end
+        
+        % Log what happened
+        switch ok
+            case 1
+                nPass = nPass + 1;
+            case 0
+                nFail = nFail + 1;
+            case -1
+                nSkip = nSkip + 1;
+        end
+	end
+    
+    % Return to root folder
+    cd(root)
+    
+% Skip Simulink tests
+else
+    % Log message
+    fprintf_echo(flog, '[%s] %s\n', ...
+        datestr(now, 'yyyy-mm-dd HH:MM:SS'), ...
+        'Skipping Simulink tests' );
+end
+
+%% Finish
+% Summary report
+fprintf_echo(flog, '[%s] %s\n', ...
+    datestr(now, 'yyyy-mm-dd HH:MM:SS'), ...
+    'Test Summary:' );
+fprintf_echo(flog, '\t%d test(s) succeeded\n', nPass);
+fprintf_echo(flog, '\t%d test(s) failed\n', nFail);
+fprintf_echo(flog, '\t%d test(s) skipped\n', nSkip);
+if nFail > 0
+    fprintf_echo(flog, '\t%s\n', ...
+        'For more information, see the log files in each test directory.')
+end
+
+% Close log file
+fclose(flog);

16a2ed8af85d799369301cdeb1ca1ac9d159294f
Stephen Frank <Stephen.Frank@nrel.gov>
2013-09-19 13:16:43 -0600

    Updated pvwatts_cosimulation demo to use new, central data folder and new DataBus functions.

--- Demo/pvwatts_cosimulation/README.txt
+++ Demo/pvwatts_cosimulation/README.txt
@@ -1,22 +1,27 @@
-This demonstration executes PVWatts cosimulation within Simulink using the MATLAB-SSC interface. The simulation uses weather data at NREL for June 2012.
+This demonstration executes a PVWatts cosimulation within Simulink using the MATLAB-SSC interface.
+The simulation uses weather data at NREL for June 2012, either from a TMY3-formatted text file or
+downloaded directly from DataBus.
 
 Features:
-•	Execute PVWatts SSC module within Simulink using the MATLAB-SSC interface
-•	Demonstrate cosimulation of PVWatts and Simulink
-•	Demonstrate generic inverter block for computing DC-AC conversion efficiency
+•   Executes PVWatts SSC module within Simulink using the MATLAB-SSC interface
+•   Demonstrates cosimulation of PVWatts and Simulink
+•   Demonstrates Simulink blocks for PV array smoothing and modeling a PV inverter
 
 Requirements:
-•	MATLAB/Simulink (R2013a)
-•	Campus Modeling project MATLAB/Simulink libraries
-•	SSC SDK
+•   MATLAB/Simulink (R2013a)
+•   Campus Energy Modeling project MATLAB/Simulink library
+•   SimPowerSystems
+•   SSC SDK
 
 Instructions: 
-1.	Open MATLAB and set the working directory to the folder that contains this README file.
-2.	Ensure the MATLAB interface folder for the SSC SDK is in the MATLAB path.
-3.	Run the script 'pvwatts_cosimulation_demo_init.m' to initialize the weather data.
-5.  Run the Simulink model and examine the results in the scope.
+1.  Open MATLAB and set the working directory to the folder that contains this README file.
+2.  Ensure the MATLAB interface folder for the SSC SDK is in the MATLAB path.
+3.  Run the script 'pvwatts_cosimulation_init.m' to initialize the weather data.
+4.  Run the Simulink model and examine the results in the scope.
 
 Comments:
-•	In addition to the Simulink model, the file 'ssc_pvwattsfunc_demo.m' provides an example of running PVWatts directly from MATLAB.
-•	The help for the 'PVWatts Cosimulation' and 'Generic Inverter' Simulink blocks contain additional information.
-•	Some aspects of the PVWatts module in SSC (pvwattsfunc) are not well understood, such as the proper way to specify a time zone. These will be resolved at a later time, at which point this demo may change.
+1.  In 'pvwatts_cosimulation_init.m', you can select either a short-term or a long-term simulation.
+    The two simulations use different weather data; see the comments in the initialization script
+	for details.
+2.  The help for the 'PVWatts Cosimulation', 'PV Smoothing', and 'Generic Inverter' Simulink blocks
+    contain additional information about how the various PV models are implemented.
---
+++ Demo/pvwatts_cosimulation/pvwatts_cosimulation_init.m
@@ -0,0 +1,119 @@
+%% PVWatts Cosimulation Demo - Initialization Script
+%
+% This script initializes the PVWatts cosimulation demo. Both short-term
+% and long-term scenarios are available. The scenarios differ by weather
+% data and simulation settings:
+%
+% 1. Short-term: This scenario models 24 hours of PV operation at a 10
+%    second time step. By default, the weather data for this scenario is
+%    downloaded from NREL's DataBus database using the batchDataBus()
+%    utility function.
+%
+% 2. Long-term: This scenario models 1 month of PV operation at a 10 minute
+%    time step. By default, the weather data is drawn from a TMY3-formatted
+%    file of NREL weather data (included with the Campus Energy Modeling
+%    library test data).
+%
+% Modify the user settings below to select a scenario, then run this script
+% prior to running the Simulink simulation. (You only need to run it once; 
+% the necessary files and settings will persist afterwards.)
+%
+% COMMENTS:
+% 1. The DC and AC powers are output as vectors to the MATLAB workspace.
+%    After running the simulation, you can plot the inverter efficiency
+%    characteristic using:
+%      eff = ac_power ./ dc_power;
+%      eff( ac_power <= 0) = 0;
+%      plot(dc_power, eff);
+%   This plot can also be created directly in Simulink, but it severly
+%   slows down the simulation time.
+% 
+% 2. This initialization script stores weather data for the short and long
+%    scenarios in the files 'Weather-short.mat' and 'Weather-long.mat',
+%    respectively. If these files already exist, the script will not
+%    recreate them. Therefore, you can easily override the weather data for
+%    each scenario by creating your own custom version of either file.
+%
+%    If you choose to do this, consult the block help for the 'Weather'
+%    block (in the Campus Energy Modeling library) and the 'From File'
+%    block (in the Simulink core library) for documentation regarding the
+%    time series structure required for the weather data.
+%
+%  3. After changing scenarios, you may need to use the Autoscale feature
+%     in the Simulink oscilloscopes to resize the scope view to fit the
+%     data.
+
+%% User Settings
+% Scenario selection: please specify either 'short' or 'long'
+scen = 'short';
+
+%% Check Inputs
+% Check for proper scenario
+scen = lower(scen);
+assert( any( strcmpi(scen, {'short','long'}) ), ...
+    'CampusModeling:demo:invalidScenario', ...
+    ['No scenario named ''' scen ''' exists; ' ...
+    'cannot initialize the demo.']);
+
+%% Initialize Weather Data
+% Data source depends on scenario
+switch scen
+    % Short-term
+    case 'short'
+        fname = 'Weather-short.mat';
+        if ~exist(fname, 'file')
+            % Time stamps for data to retrieve (yyyy-mm-dd HH:MM:SS)
+            start = '2013-06-01 00:00:00';
+            stop  = '2013-06-02 00:00:00';
+
+            % Batch import from databus -> result in 'ans'
+            sensorFile = 'DataBus_sensors.csv';
+            batchDataBus(sensorFile, start, stop, 'timezone', -6);
+            save(fname, 'ans', '-v7.3');
+        end
+    
+    % Long-term
+    case 'long'
+        fname = 'Weather-long.mat';
+        if ~exist(fname, 'file')
+            % Use convertTMY3() conversion utility -> result in 'ans'
+            dataFile = strjoin( ...
+                {'..','..','Test','data','201206ty.csv'}, filesep );
+            convertTMY3(dataFile);
+            save(fname, 'ans', '-v7.3'); 
+        end
+        
+end
+
+%% Initialize Simulink Model
+% Open it if not open
+open_system('pvwatts_cosimulation');
+
+% Set timestep and simulation duration
+switch scen
+    % Short-term - 1 day
+    case 'short'
+        timestep = 1;
+        PVtimestep = 10;
+        dur = 3600*24;
+        weatherFile = 'Weather-short.mat';
+    
+    % Long-term - 30 days
+    case 'long'
+        timestep = 15;
+        PVtimestep = 600;
+        dur = 30 * (3600*24);
+        weatherFile = 'Weather-long.mat';
+        
+end
+
+% Set settings and block parameters
+set_param('pvwatts_cosimulation', ...
+    'StartTime', num2str(0), ...
+    'StopTime', num2str(dur), ...
+    'FixedStep', num2str(timestep) );
+set_param('pvwatts_cosimulation/PVWatts Cosimulation', ...
+    'time_step', num2str(PVtimestep) );
+set_param('pvwatts_cosimulation/Weather', ...
+    'fname', weatherFile );
+save_system;

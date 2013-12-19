55709f08a0db35b7fa03ae99a0a3927683909b4d
Stephen Frank <Stephen.Frank@nrel.gov>
2013-12-19 15:49:17 -0700

    Reverted default to JRE 1.6

--- install.m
+++ install.m
@@ -40,7 +40,7 @@ installMLEP = true;
      EplusDir = 'C:\EnergyPlusV8-1-0';
  
      % Location of Java installation
-     JavaDir = 'C:\Program Files (x86)\Java\jre7\bin';
+     JavaDir = 'C:\Program Files (x86)\Java\jre6\bin';
      
 % NOTE: The MLE+ paths are ignored if installMLEP == false
 

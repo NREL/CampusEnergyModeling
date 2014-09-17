1ed4f3db796555237df829f4927c21c9d795c684
Stephen Frank <Stephen.Frank@nrel.gov>
2014-09-17 14:55:09 -0600

    Simplified and documented init script

--- Demo/dev/campus_with_central_plant/campus_with_central_plant_init.m
+++ Demo/dev/campus_with_central_plant/campus_with_central_plant_init.m
@@ -1,115 +1,44 @@
-%% setSimParam
-% This functions sets the Dialog Parameters for the different MLE+ Blocks.
-%   Willy Bernal (willyg@seas.upenn.edu)
-%   July-2013 
-fileName = 'supply_demand_campus';
+%% Campus With Central Plant - Initialization Script
+%
+% This script initializes the campus with central plant demo. The demo
+% integrates two EnergyPlus building models with a central cooling plant
+% (electric chiller) in a third EnergyPlus model.
+%
+% Run this script prior to executing the Simulink simulation. (You only
+% need to run it once; the necessary files and settings will persist
+% afterwards.)
+%
+% COMMENTS:
+% 1. The weather file, 'USA_CO_Golden-NREL.724666_TMY3.epw', should be
+%    included with your EnergyPlus installation. If not, you must change
+%    the name of the weather file to one that is included with your
+%    EnergyPlus installation.
+%
+% 2. Simulation time in this demo is based on the run period of each
+%    EnergyPlus model. The default configuration is June 1 - June 5. Time
+%    t = 0 in Simulink corresponds to midnight on June 1. The simulation
+%    stops automatically when one of the EnergyPlus models terminates; MLE+
+%    passes a termination flag output back to Simulink which ends the
+%    simulation.
 
-%% Simulation Parameters
-set_param(fileName,'SolverType', 'Fixed-step', 'FixedStep','60')
+%% Setup
+% Time step (s) - must match IDFs!
+time_step = 300;
 
-%% MLE+ Blocks
-%% E+ PLANT
-% BLOCK progname, modelfile, weatherfile, workdir, timeout, port, host, bcvtbdir, deltaT, noutputd
-blockName = 'E+ Plant';
-objName = [fileName '/' blockName];
+%% Initialize Simulink Model
+% System name
+sys = 'campus_with_central_plant';
 
-% Set Param
-param = 'progname';
-value = '''C:\EnergyPlusV8-0-0\RunEPlus.bat''';
-set_param(objName,param,value);
+% Open it if not open
+open_system(sys);
 
-% Set Param
-param = 'deltaT';
-value = '60';
-set_param(objName,param,value);
+% Set solver type and time step
+set_param(sys, ...
+    'SolverType',   'Fixed-step',       ...
+    'FixedStep',    num2str(time_step)  ...
+    );
 
-% Set Param
-param = 'weatherfile';
-value = '''USA_CO_Golden-NREL.724666_TMY3''';
-set_param(objName,param,value);
-
-% Set Param
-param = 'modelfile';
-value = '[pwd ''\ElectricChiller\ElectricChiller'']';
-set_param(objName,param,value);
-
-% Set Param
-param = 'workdir';
-value = '[pwd ''\ElectricChiller'']';
-set_param(objName,param,value);
-
-% Set Param
-param = 'noutputd';
-value = '13';
-set_param(objName,param,value);
- 
-%% E+ Building 
-% BLOCK progname, modelfile, weatherfile, workdir, timeout, port, host, bcvtbdir, deltaT, noutputd
-blockName = 'E+ Building';
-objName = [fileName '/' blockName];
-
-% Set Param
-param = 'progname';
-value = '''C:\EnergyPlusV8-0-0\RunEPlus.bat''';
-set_param(objName,param,value);
-
-% Set Param
-param = 'deltaT';
-value = '60';
-set_param(objName,param,value);
-
-% Set Param
-param = 'weatherfile';
-value = '''USA_CO_Golden-NREL.724666_TMY3''';
-set_param(objName,param,value);
-
-% Set Param
-param = 'modelfile';
-value = '[pwd ''\Building1\5ZoneAirCool'']';
-set_param(objName,param,value);
-
-% Set Param
-param = 'workdir';
-value = '[pwd ''\Building1'']';
-set_param(objName,param,value);
-
-% Set Param
-param = 'noutputd';
-value = '13';
-set_param(objName,param,value);
-
-%% E+ Building1
-% BLOCK progname, modelfile, weatherfile, workdir, timeout, port, host, bcvtbdir, deltaT, noutputd
-blockName = 'E+ Building1';
-objName = [fileName '/' blockName];
-
-% Set Param
-param = 'progname';
-value = '''C:\EnergyPlusV8-0-0\RunEPlus.bat''';
-set_param(objName,param,value);
-
-% Set Param
-param = 'deltaT';
-value = '60';
-set_param(objName,param,value);
-
-% Set Param
-param = 'weatherfile';
-value = '''USA_CO_Golden-NREL.724666_TMY3''';
-set_param(objName,param,value);
-
-% Set Param
-param = 'modelfile';
-value = '[pwd ''\Building2\5ZoneAirCool'']';
-set_param(objName,param,value);
-
-% Set Param
-param = 'workdir';
-value = '[pwd ''\Building2'']';
-set_param(objName,param,value);
-
-% Set Param
-param = 'noutputd';
-value = '13';
-set_param(objName,param,value);
-
+% Set MLE+ time step
+set_param([sys '/Central Plant'], 'time_step', num2str(time_step));
+set_param([sys '/Office Building 1'], 'time_step', num2str(time_step));
+set_param([sys '/Office Building 2'], 'time_step', num2str(time_step));
